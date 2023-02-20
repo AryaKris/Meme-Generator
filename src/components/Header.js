63ee95de0cb874622da3ed9042362a98c9be8621
Arya Krishna <dr.aryakris@outlook.com>
2023-02-20 13:34:32 -0800

    added components for Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ export default function Header() {
     return (
         <header className="header">
             <img 
-                src="./images/troll-face.png" 
-                className="header--image"
+                src="../images/troll-face.png" 
+                className="header--image" alt ="meme1"
             />
             <h2 className="header--title">Meme Generator</h2>
             <h4 className="header--project">React Course - Project 3</h4>
