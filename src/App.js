edae6cd79451b7f0ecbf5716693fe0df8d450052
Arya Krishna <dr.aryakris@outlook.com>
2023-02-20 13:50:21 -0800

    meme component with react form element

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import "./style.css";
 import Header from "./components/Header"
+import Meme from "./components/Meme"
 /**
  * Challenge: 
  * - Create a Meme component.
@@ -12,6 +13,7 @@ export default function App() {
     return (
         <div>
             <Header />
+            <Meme />
         </div>
     )
 }
